721772ee8391c2a3b512594be1b26ed02eff60ad
berquerant <sin@sin-pro.local>
2022-10-05 22:45:59 +0900

    Fix log

--- joiner/joiner.go
+++ joiner/joiner.go
@@ -114,7 +114,7 @@ func (r *relationJoiner) Join(ctx context.Context, rel *joinkey.Relation, rowC <
 	go func() {
 		defer close(resultC)
 
-		lKey, rKey := rel.Left.Add(-1, -1), rel.Right.Add(-1, -1)
+		lKey, rKey := rel.Left.Add(-1, -1), rel.Right.Add(-1, -1) // into zero-based
 		lIndex, ok := r.cache.Get(lKey.Src, lKey.Col)
 		if !ok {
 			logger.G().Warn("Join: left index not found: %v", lKey)
@@ -134,29 +134,31 @@ func (r *relationJoiner) Join(ctx context.Context, rel *joinkey.Relation, rowC <
 			if async.Done(ctx) {
 				return
 			}
-			baseInfo := func() string { return fmt.Sprintf("lkey %v rkey %v row %v", lKey, rKey, row) }
-			logger.G().Debug("Join check: %s", baseInfo())
+			baseInfo := fmt.Sprintf("lkey %v rkey %v row %v", lKey, rKey, row)
+			logger.G().Debug("Join check: %s", baseInfo)
 
 			if isTop {
 				isTop = false
 				sources = row.Keys()
 			} else if !slices.Equal(row.Keys(), sources) {
-				logger.G().Error("Join: Insonsistent rows, want %+v got %+v, %s", sources, row.Keys(), baseInfo())
+				// all rows should consist of the same sources
+				logger.G().Error("Join: Inconsistent rows, want %+v got %+v, %s", sources, row.Keys(), baseInfo)
 				return
 			}
 
+			// whether the row already contains the sources of the relation or not
 			lRow, lExist := row[lKey.Src]
 			rRow, rExist := row[rKey.Src]
 			switch {
 			case lExist && !rExist:
 				lScanned, err := lIndex.Read(lRow.Item())
 				if err != nil {
-					logger.G().Debug("Join: left read %v, %s", err, baseInfo())
+					logger.G().Debug("Join: left read %v, %s", err, baseInfo)
 					continue
 				}
 				key, err := lIndex.KeyFunc()(lScanned.Line())
 				if err != nil {
-					logger.G().Debug("Join: left key %v, %s", err, baseInfo())
+					logger.G().Debug("Join: left key %v, %s", err, baseInfo)
 					continue
 				}
 				rItemList, ok := rIndex.Get(key)
@@ -167,19 +169,19 @@ func (r *relationJoiner) Join(ctx context.Context, rel *joinkey.Relation, rowC <
 					l := row.Clone()
 					l.Set(NewSelectItem(rKey.Src, rItem))
 					logger.G().Debug("Join: from left lrow %v lline %s key %s ritem %+v, %s",
-						lRow, lScanned.Line(), key, rItem, baseInfo(),
+						lRow, lScanned.Line(), key, rItem, baseInfo,
 					)
 					resultC <- l
 				}
 			case !lExist && rExist:
 				rScanned, err := rIndex.Read(rRow.Item())
 				if err != nil {
-					logger.G().Debug("Join: right read %v, %s", err, baseInfo())
+					logger.G().Debug("Join: right read %v, %s", err, baseInfo)
 					continue
 				}
 				key, err := rIndex.KeyFunc()(rScanned.Line())
 				if err != nil {
-					logger.G().Debug("Join: right key %v, %s", err, baseInfo())
+					logger.G().Debug("Join: right key %v, %s", err, baseInfo)
 					continue
 				}
 				lItemList, ok := lIndex.Get(key)
@@ -190,29 +192,29 @@ func (r *relationJoiner) Join(ctx context.Context, rel *joinkey.Relation, rowC <
 					l := row.Clone()
 					l.Set(NewSelectItem(lKey.Src, lItem))
 					logger.G().Debug("Join: from right rrow %v rline %s key %s litem %+v, %s",
-						rRow, rScanned.Line(), key, lItem, baseInfo(),
+						rRow, rScanned.Line(), key, lItem, baseInfo,
 					)
 					resultC <- l
 				}
 			case lExist && rExist:
 				lScanned, err := lIndex.Read(lRow.Item())
 				if err != nil {
-					logger.G().Debug("Join: row left read %v, %s", err, baseInfo())
+					logger.G().Debug("Join: row left read %v, %s", err, baseInfo)
 					continue
 				}
 				lk, err := lIndex.KeyFunc()(lScanned.Line())
 				if err != nil {
-					logger.G().Debug("Join: row left key %v, %s", err, baseInfo())
+					logger.G().Debug("Join: row left key %v, %s", err, baseInfo)
 					continue
 				}
 				rScanned, err := rIndex.Read(rRow.Item())
 				if err != nil {
-					logger.G().Debug("Join: row right read %v, %s", err, baseInfo())
+					logger.G().Debug("Join: row right read %v, %s", err, baseInfo)
 					continue
 				}
 				rk, err := rIndex.KeyFunc()(rScanned.Line())
 				if err != nil {
-					logger.G().Debug("Join: row right key %v, %s", err, baseInfo())
+					logger.G().Debug("Join: row right key %v, %s", err, baseInfo)
 					continue
 				}
 				logger.G().Debug("Join: row lrow %v rrow %v lline %s rline %s lk %s rk %s",
@@ -222,7 +224,7 @@ func (r *relationJoiner) Join(ctx context.Context, rel *joinkey.Relation, rowC <
 					resultC <- row
 				}
 			default:
-				logger.G().Warn("Join: no rows found, %s", baseInfo())
+				logger.G().Warn("Join: no rows found, %s", baseInfo)
 			}
 		}
 	}()
